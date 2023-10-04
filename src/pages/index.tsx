f226f058750c4472893e283fd444e91f8c0f15b7
otkken2 <lovelovezamakko2@gmail.com>
2023-10-04 22:45:27 +0900

    firstCommit（とりあえず物件価格と固定資産税評価額から購入諸費用を概算できるようにしたところまで。）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,173 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [propertyPrice, setPropertyPrice] = useState<number| undefined>();
+  const [koteiShisanZeiHyoukaGaku, setKoteiShisanZeiHyoukaGaku] = useState<number | undefined>();
+  const [showResults, setShowResults] = useState<boolean>(false);
+  const [totalResult, setTotalResult] = useState<number>(0);
+
+  const [tyukai, setTyukai] = useState(0);
+  const [inshizei, setInshizei] = useState(0);
+  const [tourokumenkyozei, setTourokumenkyozei] = useState(0);
+  const [hudousansyutokuzei, setHudousansyutokuzei] = useState(0);
+  const SHIHOUSHOSHI_HOUSYU = 100000;
+
+  useEffect(()=>{
+    setTotalResult(tyukai+inshizei+tourokumenkyozei+hudousansyutokuzei + SHIHOUSHOSHI_HOUSYU);
+  },[hudousansyutokuzei, inshizei, tourokumenkyozei, tyukai]);
+
+  // const formatNumberWithCommas = (number: number | undefined): string => {
+  //   // カンマを削除
+  //   const targetValue = number.toString();
+  //   const numericString = targetValue.replace(/,/g, '');
+  
+  //   // 数値に変換
+  //   const numberValue = parseFloat(numericString);
+  
+  //   // NaNの場合、元の入力を返す
+  //   if (isNaN(numberValue)) return targetValue;
+  
+  //   // カンマを追加してフォーマット
+  //   return numberValue.toLocaleString('en-US');
+  // };
+
+  const calcTyukai = (): number| null => {
+    if(!propertyPrice || propertyPrice < 0)return null;
+    const taxRate = 0.05;
+    let zeinukiTesuryou: number = 0;
+    let tax: number = 0;
+
+    if(propertyPrice <= 2000000){
+        zeinukiTesuryou = propertyPrice*0.05  //物件価格の5%
+        tax = zeinukiTesuryou*taxRate; //消費税10%
+    }else if(propertyPrice <= 4000000){
+        zeinukiTesuryou = propertyPrice*0.04 + 20000  //物件価格の4%+2万円
+        tax = zeinukiTesuryou*taxRate; //消費税10%
+    }else if(propertyPrice > 4000000){
+        zeinukiTesuryou = propertyPrice*0.03 + 60000 //物件価格の3%+6万円
+        tax = zeinukiTesuryou*taxRate;
+    }
+
+    const result = zeinukiTesuryou+tax;
+    setTyukai(result);
+    return result;
+  };
+
+
+
+
+  const calcInshizei = () => {
+    if(!propertyPrice || propertyPrice < 0)return null;
+
+    let result: number = 0;
+    if(propertyPrice >= 10000 && propertyPrice <= 100000){//1万円~10万以下の場合
+      result = 200;
+    }else 
+    if (propertyPrice > 100000 && propertyPrice <= 500000){//10万〜50万以下の場合
+      result = 400;
+    }else 
+    if (propertyPrice > 500000 && propertyPrice <= 1000000){//50万~100万以下の場合
+      result = 1000;
+    }else
+    if (propertyPrice > 1000000 && propertyPrice <= 5000000){//売買価格100万〜500万以下の場合
+      result = 2000;
+    }else
+    if (propertyPrice > 5000000 && propertyPrice <= 10000000){//売買価格500万〜1千万以下の場合
+      result = 10000;
+    }else
+    if (propertyPrice > 10000000 && propertyPrice <= 50000000){//売買価格１千万~5千万以下の場合
+      result = 20000;
+    }else
+    if (propertyPrice > 50000000 && propertyPrice <= 100000000){//売買価格5千万〜1億以下の場合
+      result = 60000;
+    }else
+    if (propertyPrice > 100000000 && propertyPrice <= 500000000){//売買価格1億〜5億以下の場合
+      result = 100000;
+    }else
+    if (propertyPrice > 500000000){
+      result = 200000;
+    }
+    setInshizei(result);
+    return result;
+  };
+
+  const calcTourokumenkyozei = () => {
+    if(!koteiShisanZeiHyoukaGaku || koteiShisanZeiHyoukaGaku < 0)return;
+    const result = koteiShisanZeiHyoukaGaku*0.02;
+    setTourokumenkyozei(result);
+    return result;
+  };
+
+  const calcHudousanshutokuzei = () => {
+    if(!koteiShisanZeiHyoukaGaku || koteiShisanZeiHyoukaGaku < 0)return;
+    const result = koteiShisanZeiHyoukaGaku*0.04;
+    setHudousansyutokuzei(result);
+    return result;
+  };
+
+  const handleChangePropertyPrice = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPropertyPrice(Number(e.target.value));
+  }
+
+  const handleChangeKoteiShisanZeiHyoukaGaku = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKoteiShisanZeiHyoukaGaku(Number(e.target.value));
+  };
+
+  const handleCalcResults = () => {
+    calcTyukai();
+    calcInshizei();
+    calcTourokumenkyozei();
+    calcHudousanshutokuzei();
+    setShowResults(true);
+  };
+
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center bg-white text-black p-5`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1 className=" font-bold text-2xl">物件購入諸費用シミュレーター</h1>
+      <div className="mb-5 flex flex-col">
+        <label htmlFor="">物件価格を入力してください。</label>
+        {/* <input type="text"  value={formatNumberWithCommas(propertyPrice)} placeholder="0" className="w-[300px] text-black border-2 rounded-2xl border-black px-5 py-1" onChange={(e) => handleChangePropertyPrice(e)}/> */}
+        <input type="text"  value={propertyPrice} placeholder="0" className="w-[300px] text-black border-2 rounded-2xl border-black px-5 py-1" onChange={(e) => handleChangePropertyPrice(e)}/>
+      </div>
+      <div className="mb-5 flex flex-col">
+        <label htmlFor="">固定資産税評価額を入力してください。</label>
+        <input type="text"  value={koteiShisanZeiHyoukaGaku} placeholder="0" className="w-[300px] text-black border-2 rounded-2xl border-black px-5 py-1" onChange={(e) => handleChangeKoteiShisanZeiHyoukaGaku(e)}/>
+      </div>
+      <button onClick={() => handleCalcResults()} className="mb-20 bg-sky-400 text-white px-5 py-2 rounded-2xl w-[300px]">計算結果を表示する</button>
+      {
+        showResults && 
+        <div>
+          <div className="flex bg-red-200 w-[300px] justify-between items-center">
+            <p>仲介手数料</p>
+            <p className=" text-2xl">{tyukai}円</p>
+          </div>
+          <div className="bg-blue-300 flex justify-between w-[300px] items-center">
+            <p>印紙税</p>
+            <p className="text-2xl">{inshizei}円</p>
+          </div>
+          <div className="bg-green-300 flex w-[300px] justify-between items-center">
+            <p>司法書士報酬</p>
+            <p className="text-2xl">{SHIHOUSHOSHI_HOUSYU}円(仮)</p>
+          </div>
+          <div className="bg-yellow-300 flex justify-between w-[300px] items-center">
+            <p>登録免許税</p>
+            <p className="text-2xl ">{tourokumenkyozei}円</p>
+          </div>
+          <div className=" bg-emerald-300 flex justify-between w-[300px] items-center">
+            <p>不動産取得税</p>
+            <p className="text-2xl">{hudousansyutokuzei}円</p>
+          </div>
+
+          <div>
+            <p>諸費用合計</p>
+            <p>{totalResult}</p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      }
     </main>
   )
 }
