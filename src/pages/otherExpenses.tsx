917d9f4df75112785da695466edc938a3dfa2561
otkken2 <lovelovezamakko2@gmail.com>
2023-10-13 15:36:43 +0900

    軽微な修正

--- src/pages/otherExpenses.tsx
+++ src/pages/otherExpenses.tsx
@@ -22,41 +22,41 @@ const OtherExpenses = () => {
   return (
     <div className="p-3">
       <Header/>
-          <h2 className="text-center mt-5">まずは物件を買うときの諸費用をミエルカしよう</h2>
-    
-          <Input value={propertyPrice} onChangeFunc={(e) => handleChangePropertyPrice(e)} label="物件価格"/>
-          <Input value={koteiShisanZeiHyoukaGaku} onChangeFunc={(e) => handleChangeKoteiShisanZeiHyoukaGaku(e)} label="固定資産税評価額"/>
-          <div className="w-full flex h-fit justify-center my-8">
-            <ButtonPrimary label="計算結果を表示" onClickFunc={handleCalcOtherExpenses}/>
-          </div>
-    
-          {
-            showResults && 
-            <>
-              <ResultDisplayWrapper>
-                <div className="flex justify-between mb-3">
-                  <p>仲介手数料:</p>
-                  <p>{tyukai}万円</p>
-                </div>
-                <div className="flex justify-between mb-3">
-                  <p>印紙税:</p>
-                  <p>{inshizei}万円</p>
-                </div>
-                <div className="flex justify-between mb-3">
-                  <p>司法書士報酬:</p>
-                  <p>{SHIHOUSHOSHI_HOUSYU}万円</p>
-                </div>
-                <div className="flex justify-between mb-3">
-                  <p>登録免許税:</p>
-                  <p>{tourokumenkyozei}万円</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>不動産取得税:</p>
-                  <p>{hudousansyutokuzei}万円</p>
-                </div>
-              </ResultDisplayWrapper>
-            </>
-          }
+      <h2 className="text-center mt-5">まずは物件を買うときの諸費用をミエルカしよう</h2>
+
+      <Input value={propertyPrice} onChangeFunc={(e) => handleChangePropertyPrice(e)} label="物件価格"/>
+      <Input value={koteiShisanZeiHyoukaGaku} onChangeFunc={(e) => handleChangeKoteiShisanZeiHyoukaGaku(e)} label="固定資産税評価額"/>
+      <div className="w-full flex h-fit justify-center my-8">
+        <ButtonPrimary label="計算結果を表示" onClickFunc={handleCalcOtherExpenses}/>
+      </div>
+
+      {
+        showResults && 
+        <>
+          <ResultDisplayWrapper>
+            <div className="flex justify-between mb-3">
+              <p>仲介手数料:</p>
+              <p>{tyukai}万円</p>
+            </div>
+            <div className="flex justify-between mb-3">
+              <p>印紙税:</p>
+              <p>{inshizei}万円</p>
+            </div>
+            <div className="flex justify-between mb-3">
+              <p>司法書士報酬:</p>
+              <p>{SHIHOUSHOSHI_HOUSYU}万円</p>
+            </div>
+            <div className="flex justify-between mb-3">
+              <p>登録免許税:</p>
+              <p>{tourokumenkyozei}万円</p>
+            </div>
+            <div className="flex justify-between">
+              <p>不動産取得税:</p>
+              <p>{hudousansyutokuzei}万円</p>
+            </div>
+          </ResultDisplayWrapper>
+        </>
+      }
     </div>
   );
 };
