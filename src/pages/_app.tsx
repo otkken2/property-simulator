75915b77dc7740102736f36aff42f328921f8f98
otkken2 <lovelovezamakko2@gmail.com>
2023-10-13 16:05:05 +0900

    共通タイトルを設置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <div className='flex min-h-screen flex-col text-primary p-3'>
+      <Head>
+        <title>ミエルカ | 不動産投資の数字をミエルカするアプリ</title>
+      </Head>
       <Component {...pageProps} />
     </div>
   )
