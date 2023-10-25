f3576c42976a57fe5c5d8e1903e183aacd51d737
otkken2 <lovelovezamakko2@gmail.com>
2023-10-25 11:49:31 +0900

    amazonアソシエイトリンクの画像を貼ってみた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,8 +25,17 @@ export default function Home() {
         <Link href={"/terms"}>
           <ButtonWithBorder text="用語集" />
         </Link>
-
       </div>
+      <iframe
+          sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" 
+          style={{ width: '120px', height: '240px' ,margin: 'auto'}} 
+          // marginWidth="0" 
+          // marginHeight="0" 
+          // scrolling="no" 
+          // frameBorder="0" 
+          src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=kaitekinak056-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4815611475&linkId=10f024b4f275a0f6dd2f3857e60af918"
+        >
+        </iframe>
     </main>
   )
 }
